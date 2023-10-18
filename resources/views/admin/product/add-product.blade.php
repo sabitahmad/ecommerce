14e2d8259a25d272a2b84b032af4facea47ba40c
Monish <monishroy87@gmail.com>
2023-10-18 21:27:45 +0600

    form tag or extra added

--- resources/views/admin/product/add-product.blade.php
+++ resources/views/admin/product/add-product.blade.php
@@ -6,174 +6,175 @@
 
 @section('content')
 
-
 <div class="row g-gs">
-    <div class="col-lg-8">
-        <div class="card h-100">
-            <div class="card-inner">
-                <div class="row">
-                    <div class="col-12 mb-4">
-                        <div class="form-group">
-                            <label class="form-label" for="product-title">Product Name</label>
-                            <div class="form-control-wrap">
-                                <input type="text" class="form-control" id="product-title" placeholder="Product Name">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 mb-4">
-                        <div class="form-group">
-                            <label class="form-label" for="product-title">Product Description</label>
-                            <div class="form-control-wrap">
-                                <!-- Create the editor container -->
-                                <div class="quill-basic">
-                                    <p>Product Description</p>
+    <form class="row" action="" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="col-lg-8">
+            <div class="card h-100">
+                <div class="card-inner">
+                    <div class="row">
+                        <div class="col-12 mb-4">
+                            <div class="form-group">
+                                <label class="form-label" for="product-title">Product Name</label>
+                                <div class="form-control-wrap">
+                                    <input type="text" class="form-control" name="product_name" id="product-title" placeholder="Product Name">
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-md-6 mb-4">
-                        <div class="form-group">
-                            <label class="form-label" for="regular-price">Regular Price</label>
-                            <div class="form-control-wrap">
-                                <input type="number" class="form-control" id="regular-price" placeholder="Regular Price">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 mb-4">
-                        <div class="form-group">
-                            <label class="form-label" for="sale-price">Sale Price</label>
-                            <div class="form-control-wrap">
-                                <input type="number" class="form-control" id="sale-price" placeholder="Sale Price">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 mb-4">
-                        <div class="form-group">
-                            <label class="form-label" for="stock">Stock</label>
-                            <div class="form-control-wrap">
-                                <input type="text" class="form-control" id="stock" placeholder="Stock">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-md-6 mb-4">
-                        <div class="form-group">
-                            <label class="form-label" for="SKU">SKU</label>
-                            <div class="form-control-wrap">
-                                <input type="text" class="form-control" id="SKU" placeholder="SKU">
-                            </div>
-                        </div>
-                    </div>
-                    <div class="col-12 mb-4">
-                        <div class="form-group">
-                            <label class="form-label">Profuct Image</label>
-                            <div class="form-control-wrap">
-                                <div class="upload-zone small bg-lighter my-2">
-                                    <div class="dz-message">
-                                        <span class="dz-message-text">Drag and drop file</span>
+                        <div class="col-12 mb-4">
+                            <div class="form-group">
+                                <label class="form-label" for="product-title">Product Description</label>
+                                <div class="form-control-wrap">
+                                    <!-- Create the editor container -->
+                                    <div class="quill-basic">
+                                        <p>Product Description</p>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-12">
-                        <button class="btn btn-primary"><em class="icon ni ni-plus"></em><span>Add New</span></button>
+                        <div class="col-md-6 mb-4">
+                            <div class="form-group">
+                                <label class="form-label" for="regular-price">Regular Price</label>
+                                <div class="form-control-wrap">
+                                    <input type="number" class="form-control" name="product_price" id="regular-price" placeholder="Regular Price">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-4">
+                            <div class="form-group">
+                                <label class="form-label" for="sale-price">Sale Price</label>
+                                <div class="form-control-wrap">
+                                    <input type="number" class="form-control" name="product_sale_price" id="sale-price" placeholder="Sale Price">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-4">
+                            <div class="form-group">
+                                <label class="form-label" for="stock">Stock</label>
+                                <div class="form-control-wrap">
+                                    <input type="text" class="form-control" name="stock" id="stock" placeholder="Stock">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 mb-4">
+                            <div class="form-group">
+                                <label class="form-label" for="SKU">SKU</label>
+                                <div class="form-control-wrap">
+                                    <input type="text" class="form-control" name="sku" id="SKU" placeholder="SKU">
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 mb-4">
+                            <div class="form-group">
+                                <label class="form-label">Profuct Image</label>
+                                <div class="form-control-wrap">
+                                    <div class="upload-zone small bg-lighter my-2">
+                                        <div class="dz-message">
+                                            <span class="dz-message-text">Drag and drop file</span>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <button class="btn btn-primary"><em class="icon ni ni-plus"></em><span>Add New</span></button>
+                        </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="col-lg-4">
-        <div class="row">
-            <div class="col-12 mb-4">
-                <div class="card">
-                    <div class="card-inner">
-                        <div class="form-group">
-                            <label class="form-label">Profuct Thambnel</label>
-                            <div class="form-control-wrap">
-                                <div class="upload-zone small bg-lighter my-2">
-                                    <div class="dz-message">
-                                        <span class="dz-message-text">Drag and drop file</span>
+        <div class="col-lg-4">
+            <div class="row">
+                <div class="col-12 mb-4">
+                    <div class="card">
+                        <div class="card-inner">
+                            <div class="form-group">
+                                <label class="form-label">Profuct Thambnel</label>
+                                <div class="form-control-wrap">
+                                    <div class="upload-zone small bg-lighter my-2">
+                                        <div class="dz-message">
+                                            <span class="dz-message-text">Drag and drop file</span>
+                                        </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-12 mb-4">
-                <div class="card">
-                    <div class="card-inner">
-                        <div class="form-group">
-                            <label class="form-label">Status</label>
-                            <div class="form-control-wrap">
-                                <select class="form-select js-select2" data-placeholder="Select Status">
-                                    <option value="active">Active</option>
-                                    <option value="deactive">Deactive</option>
-                                    <option value="draft">Draft</option>
-                                </select>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="col-12 mb-4">
-                <div class="card">
-                    <div class="card-inner row">
-                        <div class="col-sm-6">
+                <div class="col-12 mb-4">
+                    <div class="card">
+                        <div class="card-inner">
                             <div class="form-group">
-                                <label class="form-label">Main Category</label>
+                                <label class="form-label">Status</label>
                                 <div class="form-control-wrap">
-                                    <select class="form-select js-select2" data-placeholder="Select Category">
-                                        @foreach ($categorys as $category)
-                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-sm-6">
-                            <div class="form-group">
-                                <label class="form-label">Sub Category</label>
-                                <div class="form-control-wrap">
-                                    <select class="form-select js-select2" multiple="multiple" data-placeholder="Select sub category">
-                                         @foreach ($sub_categorys as $category)
-                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-                                        @endforeach
+                                    <select class="form-select js-select2" data-placeholder="Select Status">
+                                        <option value="active">Active</option>
+                                        <option value="deactive">Deactive</option>
+                                        <option value="draft">Draft</option>
                                     </select>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-            </div>
-            <div class="col-12 mb-4">
-                <div class="card">
-                    <div class="card-inner">
-                        <div class="form-group">
-                            <label class="form-label">Tag</label>
-                            <div class="form-control-wrap">
-                                <div class="input-group">
-                                    <input type="text" class="form-control" placeholder="Product Tag">
-                                    <div class="input-group-prepend">
-                                        <button class="btn btn-outline-primary btn-dim">+</button>
+                <div class="col-12 mb-4">
+                    <div class="card">
+                        <div class="card-inner row">
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label class="form-label">Main Category</label>
+                                    <div class="form-control-wrap">
+                                        <select class="form-select js-select2" data-placeholder="Select Category">
+                                            @foreach ($categorys as $category)
+                                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="form-group">
+                                    <label class="form-label">Sub Category</label>
+                                    <div class="form-control-wrap">
+                                        <select class="form-select js-select2" multiple="multiple" data-placeholder="Select sub category">
+                                             @foreach ($sub_categorys as $category)
+                                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                            @endforeach
+                                        </select>
                                     </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="row g-3">
-                            <div class="col-3">
-                                <span class="badge badge-dim bg-primary fs-5">
-                                    product tag 
-                                    <button class="close"><em class="icon ni ni-cross-circle ms-1"></em></button>
-                                </span>
+                    </div>
+                </div>
+                <div class="col-12 mb-4">
+                    <div class="card">
+                        <div class="card-inner">
+                            <div class="form-group">
+                                <label class="form-label">Tag</label>
+                                <div class="form-control-wrap">
+                                    <div class="input-group">
+                                        <input type="text" class="form-control" placeholder="Product Tag">
+                                        <div class="input-group-prepend">
+                                            <button class="btn btn-outline-primary btn-dim">+</button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row g-3">
+                                <div class="col-3">
+                                    <span class="badge badge-dim bg-primary fs-6">
+                                        <span class="">×</span>&nbsp;
+                                        Tag
+                                    </span>
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        
-    </div>
+    </form>
 </div>
 
 @endsection
